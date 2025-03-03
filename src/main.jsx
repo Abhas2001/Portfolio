0dd7f579bd1c83648eb02aa93c9be4e1e5863994
Abhas Kumar <abhassinha98@gmail.com>
2025-03-04 00:49:40 +0530

    check

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+/* nfjkvfjkndvjkjkvn jktg k vkg kb ht*/
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
